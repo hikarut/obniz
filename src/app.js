9b0b2f9b88ce28df23245aae9ca85970e9f1d3a8
hikarut <ht.hikaru.takahashi@gmail.com>
2018-12-14 22:26:36 +0900

    add app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+var Obniz = require("obniz");
+
+var obniz = new Obniz("6201-0563");
+obniz.onconnect = async function () {
+  var led = obniz.wired("LED", { anode:0, cathode:1 } );
+
+  obniz.display.clear();
+  obniz.display.print("Hello World");
+}
