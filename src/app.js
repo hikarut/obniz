63e8aa12c7e06517badd0897a614429c41e90058
hikarut <ht.hikaru.takahashi@gmail.com>
2018-12-14 22:47:22 +0900

    add babel

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-var Obniz = require("obniz");
+import Obniz from 'obniz'
 
-var obniz = new Obniz("6201-0563");
-obniz.onconnect = async function () {
-  var led = obniz.wired("LED", { anode:0, cathode:1 } );
+var obniz = new Obniz('6201-0563')
+obniz.onconnect = async function() {
+  // var led = obniz.wired('LED', { anode: 0, cathode: 1 })
 
-  obniz.display.clear();
-  obniz.display.print("Hello World");
+  obniz.display.clear()
+  obniz.display.print('Hello World')
 }
